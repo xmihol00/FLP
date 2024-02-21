3e5489105d6c17f8da2972ec69f2c17ce7ecbc43
David Mihola <mihola.dejv@gmail.com>
2024-02-21 11:23:35 +0100

    main-completed cv2

--- cviko2/cv2.hs
+++ cviko2/cv2.hs
@@ -1,25 +1,31 @@
 import qualified Data.Map as Map
 import Data.List
 
+-- zapis klasickeho zip pomocí zipWith
 zip' = zipWith (\x y -> (x, y))
-
 zip'' = zipWith (,)
 
+-- filter pomocí foldr/foldl
 filter' q = foldr (\x y -> if q x then x:y else y) []
 
+-- map pomocí foldr/foldl
 map' f = foldr (\x y -> (f x) : y) []
 
-pointsL = [("aa", 1), ("bb", 0), ("cc", 5)]
+pointsL = [("xmihol00", 1), ("xborec99", 5), ("xblbec-1", 0)]
 pointsM = Map.fromList pointsL
-pM = Map.fromList [(1, "aa"), (0, "bb"), (5, "cc")]
 
+-- pomocna funkce pro overeni typu Either
 isSomething (Just _) = True 
 isSomething _ = False
 
+-- pomocna funkce pro extrahovani hodnoty z Just 
 getJust (Just a) = a
 
-activity l p = answer  
-    where found = Map.lookup l p
+-- funkce, ktera ziska pocet bodu studenta a pricte 1 a vrati (Right vysledku), 
+-- pripadne vrati (Left "max points"), pokud student ma jiz 5 bodu. 
+-- Pokud student se zadanym loginem neexistuje vrati (Left "unknown login")
+activity login students = answer  
+    where found = Map.lookup login students
           answer = if isSomething found then 
                         if (getJust found) < 5 
                         then Right ((getJust found) + 1) 
