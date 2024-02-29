71d84781a85ee09e4fe9207c32245383a2a16569
David Mihola <mihola.dejv@gmail.com>
2024-02-29 08:57:39 +0100

    main-Starting to understand read

--- proj1/main.hs
+++ proj1/main.hs
@@ -1,4 +1,24 @@
-module Main (main) where
-import Tree (BinaryTree(Node, Leaf))
+--module Main (main) where
+--import Tree (BinaryTree(Node, Leaf))
 
-main = putStrLn "Hello, World!"
+data BinaryTree = Node { key :: Int, threshold :: Float, left :: BinaryTree, right :: BinaryTree} | Leaf { value :: String } 
+    deriving (Show)
+
+indentHelper :: String -> Int -> String
+indentHelper input line =  if verified_spaces then branchHelper trimmed line else (error ("Wrong indent at line " ++ (show line) ++ ", unexpected indent."))
+    where (spaces, trimmed) = splitAt (2 * line) input
+          verified_spaces = all (==' ') spaces
+
+branchHelper ('N':'o':'d':'e':':':rest) line = rest
+branchHelper ('L':'e':'a':'f':':':rest) line = rest
+branchHelper _ _ = error ("Unexpected key word at")
+
+leafHelper input line = Leaf input
+
+--instance Read BinaryTree where
+--    readsPrec _ input = indentHelper input 0
+
+--main :: IO ()
+--main = do
+--    input <- readFile "trees/example1.txt"
+--    print $ show input
--- proj1/tree.hs
+++ proj1/tree.hs
@@ -1,6 +1,6 @@
-module Tree (BinaryTree(Node, Leaf)) where
+module Tree () where
 
-data BinaryTree a b c = Node { key :: a, threshold :: b, left :: BinaryTree a b c, right :: BinaryTree a b c} | Leaf { value :: c } 
+{-data BinaryTree a b c = Node { key :: a, threshold :: b, left :: BinaryTree a b c, right :: BinaryTree a b c} | Leaf { value :: c } 
     deriving (Show)
 instance (Read a, Read b, Read c) => Read (BinaryTree a b c) where
     readsPrec _ input = [(Leaf (reads input :: c), input)]
@@ -11,4 +11,4 @@ instance Read Point where
     readsPrec _ input =
         let [(x, rest1)] = reads input
             [(y, rest2)] = reads rest1
-        in [(Point x y, rest2)]
+        in [(Point x y, rest2)]-}
