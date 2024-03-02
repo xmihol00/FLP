479c62554925fb1ad01c8304ab7e359863d86d30
David Mihola <david.mihola@sperling.cz>
2024-03-02 18:48:40 +0100

    main-WIP - tree training

--- anagrams/anagrams.hs
+++ anagrams/anagrams.hs
@@ -4,8 +4,8 @@ import Data.Char (toLower)
 anagramsFilter :: [String] -> [String] -> [String]
 anagramsFilter [] _ = []
 anagramsFilter (x:xs) xss
-    | elem (map toLower x) (map (map toLower) xss) && not (elem (map toLower x) (map (map toLower) xs)) = x : anagramsFilter xs xss
-    | otherwise = anagramsFilter xs xss 
+    | elem (map toLower x) (map (map toLower) xss) && notElem (map toLower x) (map (map toLower) xs) = x : anagramsFilter xs xss
+    | otherwise = anagramsFilter xs xss
 
 anagramsFor :: String -> [String] -> [String]
 anagramsFor xs xss = anagramsFilter (permutations xs) (filter (\x -> (map toLower x) /= (map toLower xs)) xss)
--- proj1/main.hs
+++ proj1/main.hs
@@ -4,6 +4,8 @@ import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Read as TL
 import qualified Data.Text.IO as T
 import qualified Data.Text as T
+import qualified Data.List as L
+import qualified Data.Char as C
 
 --module Main (main) where
 --import Tree (BinaryTree(Node, Leaf))
@@ -16,7 +18,7 @@ instance Show BinaryTree where
     show tree = treeString
         where treeString = init $ showBinaryTree tree 0
 
-showBinaryTree :: BinaryTree -> Int -> [Char]
+showBinaryTree :: BinaryTree -> Int -> String
 showBinaryTree Node {..} depth = treeString
     where indent = take (2 * depth) [' ', ' ' ..]
           nextDepth = depth + 1
@@ -36,13 +38,13 @@ takeRestOfText = span (\x -> x /= '\n' && x /= ' ' && x /= '\t')
 skipWhiteSpaces :: String -> String
 skipWhiteSpaces = dropWhile (\x -> x == ' ' || x == '\t')
 
-skipWhiteSpacesAndNewLines :: [Char] -> [Char]
+skipWhiteSpacesAndNewLines :: String -> String
 skipWhiteSpacesAndNewLines = dropWhile (\x -> x == ' ' || x == '\t' || x == '\n')
 
 takeErrorSample :: String -> String
 takeErrorSample = takeWhile (/= '\n') . take 16
 
-myError :: (Show a1, Num a1) => a1 -> [Char] -> a2
+myError :: (Show a1, Num a1) => a1 -> String -> a2
 myError line text = error ("\nLine " ++ show (line + 1) ++ ": " ++ text ++ "\n")
 
 readTree :: String -> Int -> (BinaryTree, String)
@@ -90,7 +92,7 @@ traverseTree Node {..} (thresholdHead:thresholds)
 traverseTree Node {..} _ = error "Not enough values entered to perform classification."
 traverseTree Leaf {..} _ = className
 
-traverseTreeMultiple :: BinaryTree -> [[Double]] -> [Char]
+traverseTreeMultiple :: BinaryTree -> [[Double]] -> String
 traverseTreeMultiple tree (row:rows)
     | null rows = traverseTree tree row
     | otherwise = traverseTree tree row ++ "\n" ++ traverseTreeMultiple tree rows
@@ -124,6 +126,76 @@ parseValues str line
             | otherwise = myError line $ "Unexpected character at: '" ++ takeErrorSample restLine ++ "...'"
           (parsedRows, restLines) = parseValues nextRow $ line + 1
 
+gini :: [String] -> Double
+gini samples = 1 - sumOccurences
+    where classes = L.nub samples
+          samplesLen = length samples
+          occurences = [length $ filter (== x) samples | x <- classes]
+          normPow2Occurences = map (\x -> (fromIntegral x / fromIntegral samplesLen)^2) occurences
+          sumOccurences = sum normPow2Occurences
+
+giniLR :: [(String, Int)] -> [(String, Int)] -> Double
+giniLR leftSamples rightSamples = totalGini
+    where leftLen = fromIntegral $ length leftSamples
+          rightLen = fromIntegral $ length rightSamples
+          totalLen = leftLen + rightLen
+          giniLeft = gini $ map fst leftSamples
+          giniRight = gini $ map fst rightSamples
+          totalGini = leftLen * giniLeft / totalLen + rightLen * giniRight / totalLen
+
+
+split :: Char -> String -> [String]
+split delimiter strData
+    | null strData || all ((==True) . C.isSpace) strData = []
+    | otherwise = start : split delimiter rest
+    where (start, nlRest) = span (/= delimiter) strData
+          rest
+            | null nlRest = []
+            | otherwise = tail nlRest
+
+trim :: [Char] -> [Char]
+trim str = trimEnd
+    where trimmedStart = dropWhile C.isSpace str
+          trimEnd = reverse $ dropWhile C.isSpace $ reverse trimmedStart
+
+loadTrainCSV :: String -> Char -> ([[Double]], [(String, Int)])
+loadTrainCSV strData separator = (featuresDouble, trimmedClasses)
+    where colCount = length $ filter (== separator) $ takeWhile (/= '\n') strData
+          rows = split '\n' strData
+          allSameCols = all (== colCount) [length $ filter (== separator) row | row <- rows]
+          (features, classes)
+            | allSameCols = unzip [splitAt colCount $ split separator row | row <- rows]
+            | otherwise = error "Entered CSV file does not have a valid structure, some rows have more columns than other."
+          featuresDouble = L.transpose [map (\x -> fst $ doubleOrError x line) featureRow | (featureRow, line) <- zip features [0, 1 ..]]
+          trimmedClasses = [(trim $ head classRow, idx) | (classRow, idx) <- zip classes [0, 1 ..]]
+
+creteThresholds :: [[Double]] -> [[Double]]
+creteThresholds selectedFeatures = ranges
+    where sortedSelectedFeatures = map L.sort selectedFeatures
+          ranges = [zipWith (\x y -> (x+y)/2) feature (tail feature) | feature <- sortedSelectedFeatures]
+
+reduce :: (t -> t -> Bool) -> t -> [t] -> t
+reduce op best [] = best
+reduce op best [x] = if op best x then best else x
+reduce op best (x:rest) = if op best x then reduce op best rest else reduce op x rest
+
+trainTree :: ([[Double]], [(String, Int)]) -> BinaryTree
+trainTree trainingData@(features, classes)
+    | null (L.nub classNames) = error "TODO"
+    | length (L.nub classNames) == 1 = Leaf (head classNames)
+    | otherwise = Node featureIdx threshold (trainTree (features, leftClasses)) (trainTree (features, rightClasses))
+    where (classNames, indices) = unzip classes
+          selectedFeatureTable = [[feature !! index | index <- indices] | feature <- features]
+          thresholdTable = creteThresholds selectedFeatureTable
+          classesSplit = \op -> [[[classPair | (feature, classPair) <- zip selectedFeatures classes, op feature threshold] | selectedFeatures <- selectedFeatureTable, threshold <- thresholds] | thresholds <- thresholdTable]
+          leftClassesTables = classesSplit (<)
+          rightClassesTables = classesSplit (>=)
+          giniTable = [[(giniLR leftClasses rightClasses, leftClasses, rightClasses, threshold, featureIdx) | (leftClasses, rightClasses, threshold) <- zip3 leftClassesTable rightClassesTable thresholds] | (leftClassesTable, rightClassesTable, thresholds, featureIdx) <- L.zip4 leftClassesTables rightClassesTables thresholdTable [0, 1 ..]]
+          flatGini = concat giniTable
+          (gini, leftClasses, rightClasses, threshold, featureIdx) = reduce (\(x, _, _, _, _) (y, _, _, _, _) -> x < y) (head flatGini) (tail flatGini)
+
+
+
 
 --parseArgs args
 --    | length args == 2 = T.putStrLn "2 args"
@@ -136,6 +208,11 @@ main = do
     valuesInput <- readFile "values/example1.txt"
     let [(tree, _)] = reads treeInput :: [(BinaryTree, String)]
     let (values, _) = parseValues valuesInput 0
+    trainingInput <- readFile "training_data/example2.txt"
+    let trainingData = loadTrainCSV trainingInput ','
     utf8Print $ show tree
     utf8Print $ show values
     utf8Print $ traverseTreeMultiple tree values
+    utf8Print $ show trainingData
+    let tree = trainTree trainingData
+    utf8Print $ show tree
