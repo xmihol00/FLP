6aa92a3ee9dc943a7fe1db3055eedabe88e38bf5
David Mihola <mihola.dejv@gmail.com>
2024-03-27 20:07:13 +0100

    main-Hamilton cycle dulicit results

--- proj2/hamilton_cycle.pl
+++ proj2/hamilton_cycle.pl
@@ -1,5 +1,5 @@
 
-main :- parse_input(), hamilton_cycle().
+main :- parse_input(), hamilton_cycles(AC), remove_duplicates(AC, DC), writeln(DC).
 
 parse_line([A, ' ', B], A, B).
 parse_line(_, _, _) :- fail.
@@ -9,10 +9,33 @@ all_edges(Edges) :- setof([X, Y], edge(X, Y), Edges).
 
 parse_input :- read_line(L, C), 
         ( 
-            C == end_of_file ;
-	        parse_line(L, A, B), assert(edge(A, B)), assert(edge(B, A)), assert(node(A)), assert(node(B)), parse_input() ;
+            C == end_of_file;
+	        parse_line(L, A, B), assert(edge(A, B)), assert(edge(B, A)), assert(node(A)), assert(node(B)), parse_input();
             parse_input()
 	    ).
 
-hamilton_cycle :- all_nodes(Nodes), permutation(Nodes, P), check_cycle(P), writeln(P).
-check_cycle([A, B | T]) :- edge(A, B), check_cycle([B | T]).
+first_same_permutations(Permutations) :-
+    all_nodes([A | T]),
+    findall(P, permutation(T, P), PU),
+    maplist({A}/[L, [A|L]]>>true, PU, Permutations).
+
+hamilton_cycles(Cycles) :- first_same_permutations(P), include(is_cycle, P, Cycles).
+
+is_cycle([A | T]) :- is_cycle([A | T], A).
+is_cycle([A, B | T], X) :- edge(A, B), is_cycle([B | T], X). 
+is_cycle([A], X) :- edge(A, X).
+
+remove_duplicates(AllCycles, DedupCycles) :- 
+    maplist([[_|Rest], Rest]>>true, AllCycles, DedupCycles),
+    inner_list_length(DedupCycles, L),
+    (
+        L /\ 1 =:= 1 -> 
+            writeln("Odd"); 
+            writeln("Even")
+    ),
+    Middle is L // 2,
+    writeln(Middle).
+
+// TODO group by
+
+inner_list_length([A|T], L) :- length(A, L).
