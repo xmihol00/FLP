e7dfa066fd2583048d94c014b5ddcf4c5ad1882a
David Mihola <mihola.dejv@gmail.com>
2024-03-28 13:51:01 +0100

    main-WIP - possibly 1st working Hamilton cycle implementation

--- proj2/hamilton_cycle.pl
+++ proj2/hamilton_cycle.pl
@@ -1,5 +1,5 @@
 
-main :- parse_input(), hamilton_cycles(AC), remove_duplicates(AC, DC), writeln(DC).
+main :- parse_input(), hamilton_cycles(AC), writeln(AC).
 
 parse_line([A, ' ', B], A, B).
 parse_line(_, _, _) :- fail.
@@ -19,26 +19,45 @@ first_same_permutations(Permutations) :-
     findall(P, permutation(T, P), PU),
     maplist({A}/[L, [A|L]]>>true, PU, Permutations).
 
-hamilton_cycles(Cycles) :- first_same_permutations(P), include(is_cycle, P, Cycles).
+hamilton_cycles(Cycles) :- make_permutations(P), include(is_cycle, P, Cycles).
 
 is_cycle([A | T]) :- is_cycle([A | T], A).
 is_cycle([A, B | T], X) :- edge(A, B), is_cycle([B | T], X). 
 is_cycle([A], X) :- edge(A, X).
 
-remove_duplicates(AllCycles, DedupCycles) :- 
-    maplist([[_|Rest], Rest]>>true, AllCycles, Tails),
-    [[HH|_]|_] = AllCycles,
-    inner_list_length(Tails, L),
+insertUntil(Element, N, Xs, Ys) :-
+    append(Start, End, Xs),
+    length(Start, M),
+    N > M,
+    append(Start, [Element|End], Ys).
+
+flatten([], []).
+flatten([H|T], Flat) :- flatten(T, FlatT), append(H, FlatT, Flat).
+
+make_permutations(UniquePerms) :- 
+    all_nodes(List), 
+    length(List, Len),
     (
-        L /\ 1 =:= 1 -> 
-            M is L // 2, writeln(Tails), 
-                create_tuples(M, Tails, Tups), 
-                findall(Group, group_by(Key, Value, member((Key, Value), Tups), Group), Groups), 
-                maplist([[H|_], H]>>true, Groups, XX),
-                maplist({HH}/[[U | T], [HH, U | T]]>>true, XX, DedupCycles);
-            writeln("Even")
+        Len /\ 1 =:= 1 -> 
+            [Head1, Head2 | Tail] = List,
+                Half is (Len) // 2,
+                findall(P, permutation(Tail, P), Perms), 
+                maplist({Half, Head2}/[L, Inserted]>>(findall(Ys, insertUntil(Head2, Half, L, Ys), Inserted)), Perms, Lists),
+                flatten(Lists, FlattenLists),
+                maplist({Head1}/[[H|T], Concatenated]>>(Concatenated=[Head1, H | T]), FlattenLists, UniquePerms);
+            [Head1, Head2, Head3 | Tail] = List,
+                Half is (Len-1) // 2,
+                findall(P, permutation(Tail, P), Perms), 
+                maplist({Half, Head3}/[L, Inserted]>>(findall(Ys, insertUntil(Head3, Half, L, Ys), Inserted)), Perms, Lists),
+                flatten(Lists, FlattenLists),
+                HalfPlus is Len-1,
+                maplist({Head2}/[[H|T], Concatenated]>>(Concatenated=[Head2, H | T]), FlattenLists, MappedLists),
+                maplist({HalfPlus, Head1}/[L, Inserted]>>(findall(Ys, insertUntil(Head1, HalfPlus, L, Ys), Inserted)), MappedLists, InsertedAgain),
+                flatten(InsertedAgain, UniquePerms)
     ).
 
 create_tuples(N, Lists, Tuples) :- maplist({N}/[List, Tuple]>>(nth0(N, List, Elem), Tuple = (Elem, List)), Lists, Tuples).
 
 inner_list_length([A|_], L) :- length(A, L).
+
+min_list
--- proj2/tmp.pl
+++ proj2/tmp.pl
@@ -9,7 +9,7 @@ student_grade(eve, 95).
 create_tuples(N, Lists, Tuples) :- maplist({N}/[List, Tuple]>>(nth0(N, List, Elem), Tuple = (Elem, List)), Lists, Tuples).
 
 create_lists(L) :- L = [[a,1,u],[b,2,v],[c,1,x],[d,2,z],[e,3,zz]].
-create_list(L) :- L = [1, 2, 3, 4, 5].
+create_list(L) :- L = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"].
 
 gps(G) :- create_lists(L), create_tuples(1, L, Tups), group_by(A, T, member((A, T), Tups), G).
 
@@ -24,17 +24,25 @@ flatten([H|T], Flat) :- flatten(T, FlatT), append(H, FlatT, Flat).
 
 make_permutations(UniquePerms) :- 
     create_list(List), 
-    length(Tail, LenT),
-    Len /\ 1 =:= 1 -> 
-        [Head1, Head2 | Tail] = List,
-            length(Tail, LenT),
-            Half is (LenT+1) // 2,
-            findall(P, permutation(Tail, P), Perms), 
-            maplist({Half, Head2}/[L, Inserted]>>(findall(Ys, insertUntil(Head2, Half, L, Ys), Inserted)), Perms, Lists),
-            flatten(Lists, FlattenLists),
-            writeln(FlattenLists),
-            maplist({Head1}/[[H|T], Concatenated]>>(Concatenated=[Head1, H | T]), FlattenLists, UniquePerms);
-        writeln(List), UniquePerms=List.
+    length(List, Len),
+    (
+        Len /\ 1 =:= 1 -> 
+            [Head1, Head2 | Tail] = List,
+                Half is (Len) // 2,
+                findall(P, permutation(Tail, P), Perms), 
+                maplist({Half, Head2}/[L, Inserted]>>(findall(Ys, insertUntil(Head2, Half, L, Ys), Inserted)), Perms, Lists),
+                flatten(Lists, FlattenLists),
+                maplist({Head1}/[[H|T], Concatenated]>>(Concatenated=[Head1, H | T]), FlattenLists, UniquePerms);
+            [Head1, Head2, Head3 | Tail] = List,
+                Half is (Len-1) // 2,
+                findall(P, permutation(Tail, P), Perms), 
+                maplist({Half, Head3}/[L, Inserted]>>(findall(Ys, insertUntil(Head3, Half, L, Ys), Inserted)), Perms, Lists),
+                flatten(Lists, FlattenLists),
+                HalfPlus is Len-1,
+                maplist({Head2}/[[H|T], Concatenated]>>(Concatenated=[Head2, H | T]), FlattenLists, MappedLists),
+                maplist({HalfPlus, Head1}/[L, Inserted]>>(findall(Ys, insertUntil(Head1, HalfPlus, L, Ys), Inserted)), MappedLists, InsertedAgain),
+                flatten(InsertedAgain, UniquePerms)
+    ).
 
 unique_perms() :- make_permutations(A), writeln(A), length(A, L), writeln(L).
 
@@ -42,4 +50,6 @@ unique_perms() :- make_permutations(A), writeln(A), length(A, L), writeln(L).
     ins(Lists) :- maplist([Idx, List]>>(insertAt(1, Idx, [a,b,c,d,e], List)), [0, 1, 2], Lists). 
     findall(Ys, insertUntil(1,2,[a,b,c,d,e],Ys), Lists)
     maplist([L, Inserted]>>(findall(Ys, insertUntil(6, 2, L, Ys), Inserted)), [[a,1,u],[b,2,v]], Lists).
+    [[3,4], [4,3]] [[2,3,4], [3,2,4], [2,4,3], [4,2,3]
+    [[1,2,3,4],[1,3,2,4],[1,4,3,2]]
  */
