4080d5accb5423bf61f4c44da05d2e2c2ae664ea
David Mihola <david.mihola@sperling.cz>
2024-04-02 09:03:43 +0200

    main-performance tests

--- proj2/performance_graphs/analyze_performance.py
+++ proj2/performance_graphs/analyze_performance.py
@@ -7,24 +7,32 @@ import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-r", "--rerun_all_tests", action="store_true", help="Rerun all tests.")
+parser.add_argument("-t", "--timeout", type=int, default=60, help="Timeout for each test.")
 
 args = parser.parse_args()
 
-if os.path.exists("performance_results.csv") and not args.rerun_all_tests:
-    df = pd.read_csv("performance_results.csv")
-    df.set_index("filename", inplace=True)
-else:
-    df = pd.DataFrame(columns=["filename", "test_type", "time", "ret_code"])
-    df.set_index("filename", inplace=True)
 
 for suffix in ["circle.txt", "fully_connected.txt"]:
     test_type = suffix.split(".")[0]
-    for filename in glob(f"*{suffix}"):
+    csv_name = f"performance_results_{test_type}.csv"
+    if os.path.exists(csv_name) and not args.rerun_all_tests:
+        df = pd.read_csv(csv_name)
+        df.set_index("file_name", inplace=True)
+    else:
+        df = pd.DataFrame(columns=["file_name", "test_type", "time", "result"])
+        df.set_index("file_name", inplace=True)
+
+    for filename in sorted(list(glob(f"*{suffix}"))):
         if filename not in df.index:
             start = time()
-            ret_code = subprocess.run(f"../test-flp23-log <{filename} >/dev/null 2>/dev/null", shell=True, timeout=300)
+            try:
+                ret_code = subprocess.run(f"timeout {args.timeout + 1} ../test-flp23-log <{filename} >/dev/null 2>/dev/null", shell=True, timeout=args.timeout)
+                ret_message = "Success" if ret_code.returncode == 0 else "Failed"
+            except subprocess.TimeoutExpired:
+                ret_code = None
+                ret_message = "Timeout"
             end = time()
-            df.loc[filename] = [test_type, end - start, ret_code.returncode]
-            df.to_csv("performance_results.csv")
-            print(f"{filename} took {end - start} seconds, return code {ret_code.returncode}")
-            sleep(1)
+            df.loc[filename] = [test_type, end - start, ret_message]
+            df.to_csv(csv_name)
+            print(f"{filename} took {end - start} seconds with {ret_message}")
+            sleep(2)
