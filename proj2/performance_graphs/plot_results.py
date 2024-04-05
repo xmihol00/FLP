e4217a4fd31cae24ae58f412803c7ea7ca48f769
David Mihola <david.mihola@sperling.cz>
2024-04-05 08:23:28 +0200

    main-improved plots

--- proj2/performance_graphs/plot_results.py
+++ proj2/performance_graphs/plot_results.py
@@ -9,23 +9,23 @@ marker_dict = {
     "Timeout": "$T$"
 }
 
-fig, axes = plt.subplots(2, 2, figsize=(14, 8))
-axes = axes.flatten()
-for ax, test_type in zip(axes, ["circle", "fully_connected", "2D_mesh", "2D_wrap-around_mesh"]):
+for test_type in ["circle", "fully_connected", "2D_mesh", "2D_wrap-around_mesh"]:
+    plt.figure(figsize=(12, 8))
     for results_filename, marker, color in zip(sorted(glob(f"*_{test_type}_results.csv")), ['o', 's', 'D', '*'], mcolors.TABLEAU_COLORS):
         marker_dict["Success"] = marker
         df = pd.read_csv(results_filename)
-        markers = [marker_dict[result] for result in df["result"]]
+        max_time = df["time"].max()
         for row in df.itertuples():
-            ax.plot(row.Index, row.time, marker=marker_dict[row.result], markersize=6, color=color, alpha=0.75)
+            plt.plot(row.Index, row.time, marker=marker_dict[row.result], markersize=10, color=color)
         df.set_index("graph_name", inplace=True)
-        ax.plot(df["time"], label=results_filename.split("_")[0], marker='', linestyle='--', markersize=0, color=color, alpha=0.75)
-    ax.set_title(f"{test_type.replace('_', ' ').capitalize()} graph performance comparison")
-    ax.set_xlabel("nodes")
-    ax.set_yscale("log")
-    ax.set_ylabel("time [s]")
-    ax.grid()
-    
-plt.legend()
-plt.tight_layout()
-plt.savefig(f"performance_results.png", dpi=400)
+        plt.plot(df["time"], label=results_filename.split("_")[0], marker='', linestyle='--', markersize=0, color=color)
+    plt.title(f"{test_type.replace('_', ' ').capitalize()} graph performance comparison")
+    plt.xlabel("nodes")
+    plt.yscale("log")
+    plt.ylabel("time (s)")
+    #plt.xlim(-0.1, max_time * 1.1)
+    plt.scatter([list(df.itertuples())[0].Index], [-5], marker="$F$", color="black", label="Fail")
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig(f"{test_type}_results.png", dpi=400)
