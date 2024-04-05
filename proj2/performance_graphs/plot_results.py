71c8e24a32d318e173da5c28c80b779c3a7e19d2
David Mihola <david.mihola@sperling.cz>
2024-04-05 08:07:57 +0200

    main-more performance results

--- proj2/performance_graphs/plot_results.py
+++ proj2/performance_graphs/plot_results.py
@@ -9,21 +9,23 @@ marker_dict = {
     "Timeout": "$T$"
 }
 
-for test_type in ["circle", "fully_connected", "2D_mesh", "2D_wrap-around_mesh"]:
-    plt.figure(figsize=(12, 8))
+fig, axes = plt.subplots(2, 2, figsize=(14, 8))
+axes = axes.flatten()
+for ax, test_type in zip(axes, ["circle", "fully_connected", "2D_mesh", "2D_wrap-around_mesh"]):
     for results_filename, marker, color in zip(sorted(glob(f"*_{test_type}_results.csv")), ['o', 's', 'D', '*'], mcolors.TABLEAU_COLORS):
         marker_dict["Success"] = marker
         df = pd.read_csv(results_filename)
         markers = [marker_dict[result] for result in df["result"]]
         for row in df.itertuples():
-            plt.plot(row.Index, row.time, marker=marker_dict[row.result], markersize=10, color=color)
+            ax.plot(row.Index, row.time, marker=marker_dict[row.result], markersize=6, color=color, alpha=0.75)
         df.set_index("graph_name", inplace=True)
-        plt.plot(df["time"], label=results_filename.split("_")[0], marker='', linestyle='--', markersize=0, color=color)
-    plt.title(f"{test_type.replace('_', ' ').capitalize()} graph performance comparison")
-    plt.xlabel("nodes")
-    plt.yscale("log")
-    plt.ylabel("time (s)")
-    plt.legend()
-    plt.grid()
-    plt.tight_layout()
-    plt.savefig(f"{test_type}_results.png", dpi=400)
+        ax.plot(df["time"], label=results_filename.split("_")[0], marker='', linestyle='--', markersize=0, color=color, alpha=0.75)
+    ax.set_title(f"{test_type.replace('_', ' ').capitalize()} graph performance comparison")
+    ax.set_xlabel("nodes")
+    ax.set_yscale("log")
+    ax.set_ylabel("time [s]")
+    ax.grid()
+    
+plt.legend()
+plt.tight_layout()
+plt.savefig(f"performance_results.png", dpi=400)
