e7d77b04ca6cd1f8b4b8e21bd12da45ff3746a9c
David Mihola <david.mihola@sperling.cz>
2024-05-03 13:17:03 +0200

    main-fixed double linked list

--- exam_prep/double_linked_list.hs
+++ exam_prep/double_linked_list.hs
@@ -1,100 +1,53 @@
 import Data.Maybe (isJust, isNothing)
 
-data DoubleLinkedList v = DoubleLinkedList v (Maybe (DoubleLinkedList v)) (Maybe (DoubleLinkedList v))
-    deriving (Eq, Show)
+data DLL a = DLL { previous :: Maybe (DLL a), value :: a, next :: Maybe (DLL a) }
 
-isSomething :: Maybe a -> Bool
-isSomething (Just _) = True
-isSomething _ = False
+nextShow :: (Show a) => DLL a -> String
+nextShow (DLL _ v Nothing) = show v
+nextShow (DLL _ v (Just n)) = show v ++ " -> " ++ nextShow n
 
-printAllNext :: Show a => Maybe (DoubleLinkedList a) -> String
-printAllNext head
-    | isJust head = show value ++ ' ':printAllNext next
-    | otherwise = ""
+previousShow :: (Show a) => DLL a -> String
+previousShow (DLL Nothing v _) = show v
+previousShow (DLL (Just p) v _) = show v ++ " -> " ++ previousShow p
+
+dllShow :: (Show a) => (Maybe (DLL a), Maybe (DLL a)) -> String
+dllShow (head, tail)
+    | isNothing head || isNothing tail = "Empty"
+    | otherwise = "head to tail: " ++ nextShow h ++ "\ntail to head: " ++ previousShow t
+    where 
+        Just h = head
+        Just t = tail
+
+dllFromList :: [a] -> (Maybe (DLL a), Maybe (DLL a))
+dllFromList [] = (Nothing, Nothing)
+dllFromList [x] = (firstNode, firstNode)
     where
-        (Just justHead) = head
-        (DoubleLinkedList value next _) = justHead
-
-printAllPrevious :: Show a => Maybe (DoubleLinkedList a) -> [Char]
-printAllPrevious tail
-    | isJust tail = show value ++ ' ':printAllPrevious previous
-    | otherwise = ""
+        firstNode = Just (DLL Nothing x Nothing)
+dllFromList (x:y:xs) = (Just firstNode, Just lastNode)
     where
-        (Just justTail) = tail
-        (DoubleLinkedList value _ previous) = justTail
+        firstNode = DLL Nothing x (Just secondNode)
+        (secondNode, lastNode) = buildDLL firstNode y xs
 
-foldrUpdateAcc :: v -> Maybe (DoubleLinkedList v) -> Maybe (DoubleLinkedList v)
-foldrUpdateAcc element accumulator
-    | isNothing accumulator = Just (DoubleLinkedList element Nothing Nothing)
-    | otherwise = newAcc
+buildDLL :: DLL a -> a -> [a] -> (DLL a, DLL a)
+buildDLL previousNode value [] = (node, node)
+    where 
+        node = DLL (Just previousNode) value Nothing
+buildDLL previousNode value (x:xs) = (node, lastNode)
     where
-        (Just justAcc) = accumulator
-        (DoubleLinkedList accValue accPrevious accNext) = justAcc
-        oldAcc = Just (DoubleLinkedList accValue accPrevious newAcc)
-        newAcc = Just (DoubleLinkedList element oldAcc Nothing)
+        node = DLL (Just previousNode) value (Just nextNode)
+        (nextNode, lastNode) = buildDLL node x xs
 
-foldlUpdateAcc :: Maybe (DoubleLinkedList v) -> v -> Maybe (DoubleLinkedList v)
-foldlUpdateAcc accumulator element
-    | isNothing accumulator = Just (DoubleLinkedList element Nothing Nothing)
-    | otherwise = newAcc
+dll :: (Maybe (DLL Integer), Maybe (DLL Integer))
+dll = dllFromList [1 .. 10]
+
+dllHead :: DLL Integer
+dllHead = node
     where
-        (Just justAcc) = accumulator
-        (DoubleLinkedList accValue accPrevious accNext) = justAcc
-        oldAcc = Just (DoubleLinkedList accValue newAcc accNext)
-        newAcc = Just (DoubleLinkedList element Nothing oldAcc)
-
-linkList :: [Maybe (DoubleLinkedList v)] -> [Maybe (DoubleLinkedList v)]
-linkList [x, y] = [newJustX, newJustY]
+        Just node = fst dll
+dllTail :: DLL Integer
+dllTail = node
     where
-        (Just justX) = x
-        (DoubleLinkedList valueX nextX previousX) = justX
-        (Just justY) = y
-        (DoubleLinkedList valueY nextY previousY) = justY
-        newJustX = Just (DoubleLinkedList valueX newJustY previousX)
-        newJustY = Just (DoubleLinkedList valueY nextY newJustX)
+        Just node = snd dll
 
-linkList (x:y:rest) = newJustX:linkList (newJustY:rest)
-    where
-        (Just justX) = x
-        (DoubleLinkedList valueX nextX previousX) = justX
-        (Just justY) = y
-        (DoubleLinkedList valueY nextY previousY) = justY
-        newJustX = Just (DoubleLinkedList valueX newJustY previousX)
-        newJustY = Just (DoubleLinkedList valueY nextY newJustX)
-linkList _ = []
-
---dllFromList :: [a] -> (Maybe (DoubleLinkedList a), Maybe (DoubleLinkedList a))
---dllFromList list = (dllHead, dllTail)
---    where
---        dllTail = foldl foldlUpdateAcc Nothing list
---        getHead tail
---            | isJust tail && isJust previous = getHead previous
---            | otherwise = tail
---            where
---                (Just justTail) = tail
---                (DoubleLinkedList value _ previous) = justTail
---        dllHead = getHead dllTail
-dllFromList :: [a] -> (Maybe (DoubleLinkedList a), Maybe (DoubleLinkedList a))
-dllFromList list = (dllHead, dllTail)
-    where
-        mapped = map (\x -> Just (DoubleLinkedList x Nothing Nothing)) list
-        linked = linkList mapped
-        dllHead = head linked
-        dllTail = last linked
-
-
-dll :: (Maybe (DoubleLinkedList Integer), Maybe (DoubleLinkedList Integer))
-dll = dllFromList [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-dllHead :: Maybe (DoubleLinkedList Integer)
-dllHead = fst dll
-dllTail :: Maybe (DoubleLinkedList Integer)
-dllTail = snd dll
-
-dllThirdToLast :: Maybe (DoubleLinkedList Integer)
-dllThirdToLast = thirdToLast
-    where
-        (Just justTail) = dllTail
-        (DoubleLinkedList _ _ (Just justSecondToLast)) = justTail
-        (DoubleLinkedList _ _ (Just justThirdToLast)) = justSecondToLast
-        (DoubleLinkedList _ _ thirdToLast) = justThirdToLast
+dllPrint :: IO ()
+dllPrint = putStrLn $ dllShow dll
